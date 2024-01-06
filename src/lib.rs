43cf2c2117a18c57732c7c31a782d662f4be4786
Chris Hennick <hennickc@amazon.com>
2024-01-05 17:27:23 -0800

    Fix unit tests (main.rs still needs work)

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,12 @@
-#[cfg(not(loom))]
-use std::{sync::{Arc, atomic::{AtomicUsize}}, thread::{spawn, yield_now}, cell::UnsafeCell};
+use std::{sync::{Arc, atomic::{AtomicUsize}}, thread::{spawn, yield_now}};
 use std::fmt::Debug;
 use std::slice;
-use async_channel::{Receiver};
-use log::{error, info};
+use async_channel::{Receiver, Sender};
+use log::{info};
 use rand::Rng;
 use rand::rngs::OsRng;
 use rand_core::{CryptoRng, RngCore};
 use rand_core::block::{BlockRng64, BlockRngCore};
-
-#[derive(Debug)]
-#[repr(transparent)]
-pub(crate) struct SyncUnsafeCell<T>(UnsafeCell<T>);
-
-// Reimplemented so that we can use same API in loom
-impl<T> SyncUnsafeCell<T> {
-    #[allow(unused)]
-    #[cfg(not(loom))]
-    pub(crate) unsafe fn with<R>(&self, f: impl FnOnce(&T) -> R) -> R {
-        f(self.0.get().as_ref().unwrap())
-    }
-
-    pub(crate) unsafe fn with_mut<R>(&self, f: impl FnOnce(&mut T) -> R) -> R {
-        f(self.0.get().as_mut().unwrap())
-    }
-
-}
-
-impl <T> From<T> for SyncUnsafeCell<T> {
-    fn from(value: T) -> Self {
-        SyncUnsafeCell(UnsafeCell::new(value))
-    }
-}
-
-unsafe impl <T> Sync for SyncUnsafeCell<T> {}
-
-#[derive(Debug)]
-struct SharedBufferRngInner<const WORDS_PER_SEED: usize, const SEEDS_CAPACITY: usize> {
-    receiver: Receiver<[u64; WORDS_PER_SEED]>,
-}
-
-unsafe impl <const WORDS_PER_SEED: usize, const SEEDS_CAPACITY: usize> Sync
-for SharedBufferRngInner<WORDS_PER_SEED, SEEDS_CAPACITY> {}
 
 pub struct DefaultableArray<const N: usize, T>([T; N]);
 
@@ -63,14 +28,11 @@ impl <const N: usize, T> AsRef<[T]> for DefaultableArray<N, T> {
     }
 }
 
-#[derive(Debug)]
-pub struct SharedBufferRng<const WORDS_PER_SEED: usize, const SEEDS_CAPACITY: usize>
-(Arc<SyncUnsafeCell<SharedBufferRngInner<WORDS_PER_SEED, SEEDS_CAPACITY>>>);
-
-impl <const WORDS_PER_SEED: usize, const SEEDS_CAPACITY: usize> Clone for SharedBufferRng<WORDS_PER_SEED, SEEDS_CAPACITY> {
-    fn clone(&self) -> Self {
-        SharedBufferRng(self.0.clone())
-    }
+#[derive(Clone, Debug)]
+pub struct SharedBufferRng<const WORDS_PER_SEED: usize, const SEEDS_CAPACITY: usize> {
+    // Needed to keep the weak sender reachable as long as the receiver is strongly reachable
+    sender: Arc<Sender<[u64; WORDS_PER_SEED]>>,
+    receiver: Receiver<[u64; WORDS_PER_SEED]>
 }
 
 pub type SharedBufferRngStd = SharedBufferRng<8, 16>;
@@ -81,23 +43,19 @@ SharedBufferRng<WORDS_PER_SEED, SEEDS_CAPACITY> {
         BlockRng64::new(Self::new(inner))
     }
 
-    pub fn new<T: Rng + Send + Debug + 'static>(inner_inner: T) -> Self {
+    pub fn new<T: Rng + Send + Debug + 'static>(mut inner_inner: T) -> Self {
         let (sender, receiver) = async_channel::bounded(SEEDS_CAPACITY);
         info!("Creating a SharedBufferRngInner for {:?}", inner_inner);
-        let inner_inner = SyncUnsafeCell::from(inner_inner);
-        let inner = Arc::new(SyncUnsafeCell::from(SharedBufferRngInner {
-            receiver
-        }));
-        let weak_sender = sender.downgrade();
+        let inner = receiver.clone();
+        let weak_sender = sender.clone().downgrade();
         spawn(move || {
             let mut seed_from_source = [0; WORDS_PER_SEED];
             loop {
                 match weak_sender.upgrade() {
                     None => return,
                     Some(sender) => unsafe {
-                        inner_inner.with_mut(
-                            |inner_inner| seed_from_source.iter_mut().for_each(
-                                |word| *word = inner_inner.next_u64()));
+                        seed_from_source.iter_mut().for_each(
+                                |word| *word = inner_inner.next_u64());
                         while !sender.send_blocking(seed_from_source).is_ok() {
                             if weak_sender.upgrade().is_none() {
                                 return;
@@ -108,7 +66,13 @@ SharedBufferRng<WORDS_PER_SEED, SEEDS_CAPACITY> {
                 }
             }
         });
-        SharedBufferRng(inner)
+        while (receiver.is_empty()) {
+            yield_now();
+        }
+        SharedBufferRng {
+            receiver: inner,
+            sender: sender.into()
+        }
     }
 }
 
@@ -119,21 +83,12 @@ for SharedBufferRng<WORDS_PER_SEED, SEEDS_CAPACITY> {
 
     fn generate(&mut self, results: &mut Self::Results) {
         let results: &mut [[u64; WORDS_PER_SEED]] = slice::from_mut(&mut results.0).into();
-        unsafe {
-            loop {
-                self.0.with(|inner|
-                    match inner.receiver.recv_blocking() {
-                        Ok(seed) => {
-                            results[0].copy_from_slice(&seed);
-                            return;
-                        },
-                        Err(e) => {
-                            error!("Error from recv_blocking(): {}", e);
-                            yield_now();
-                        }
-                    }
-                );
-            }
+        match self.receiver.recv_blocking() {
+            Ok(seed) => {
+                results[0].copy_from_slice(&seed);
+                return;
+            },
+            Err(e) => panic!("Error from recv_blocking(): {}", e)
         }
     }
 }
@@ -142,11 +97,11 @@ impl <const WORDS_PER_SEED: usize, const SEEDS_CAPACITY: usize> CryptoRng for Sh
 
 #[cfg(test)]
 mod tests {
-    #[cfg(not(loom))]
     use core::sync::atomic::{AtomicUsize, Ordering::SeqCst};
+    use std::sync::OnceLock;
     use rand_core::block::{BlockRng64, BlockRngCore};
-    #[cfg(not(loom))]
     use rand_core::{Error};
+    use scc::Bag;
     use super::*;
 
     const U8_VALUES: usize = u8::MAX as usize + 1;
@@ -178,5 +133,35 @@ mod tests {
         let zero_seed_prng = StdRng::from_seed([0; 32]);
         assert_ne!(client_prng, zero_seed_prng);
         Ok(())
+    }
+
+    static WORDS: OnceLock<Bag<u64>> = OnceLock::new();
+
+    #[test_log::test]
+    fn loom_test_at_most_once_delivery() {
+        WORDS.get_or_init(Bag::new);
+        use rand::RngCore;
+        const THREADS: usize = 2;
+        const ITERS_PER_THREAD: usize = 1;
+        let seeder: SharedBufferRng::<8,4> = SharedBufferRng::new(BlockRng64::new(
+            ByteValuesInOrderRng { words_written: AtomicUsize::new(0)}));
+        let ths: Vec<_> = (0..THREADS)
+            .map(|_| {
+                let mut seeder_clone = BlockRng64::new(seeder.clone());
+                spawn(move || {
+                    for _ in 0..ITERS_PER_THREAD {
+                        WORDS.get().unwrap().push(seeder_clone.next_u64());
+                    }
+                })
+            })
+            .collect();
+        for th in ths {
+            th.join().unwrap();
+        }
+        let mut words_sorted = Vec::new();
+        WORDS.get().unwrap().pop_all((), |(), word| words_sorted.push(word));
+        let mut words_dedup = words_sorted.clone();
+        words_dedup.dedup();
+        assert_eq!(words_dedup.len(), words_sorted.len());
     }
 }
