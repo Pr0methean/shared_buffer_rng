1561160baf1161a9ae639521bf9d1cf566573e17
Chris Hennick <hennickc@amazon.com>
2024-01-19 23:51:18 -0800

    Refactor: RngBufferCore doesn't have to align its unshared buffer

--- benches/bench.rs
+++ benches/bench.rs
@@ -6,7 +6,7 @@ use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criteri
 use rand::rngs::adapter::ReseedingRng;
 use rand_chacha::{ChaCha12Core};
 use rand_core::{OsRng, RngCore, SeedableRng};
-use shared_buffer_rng::{DefaultableAlignedArray, SharedBufferRng, WORDS_PER_STD_RNG};
+use shared_buffer_rng::{SharedBufferRng, WORDS_PER_STD_RNG};
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering::SeqCst;
 use std::thread::spawn;
@@ -16,11 +16,44 @@ use rand_core::block::{BlockRng64, BlockRngCore};
 
 const RESEEDING_THRESHOLD: u64 = 1024;
 
+#[derive(Copy, Clone)]
+struct DefaultableUnalignedArray<const N: usize, T>([T; N]);
+
+impl <const N: usize, T: Default + Copy> Default for DefaultableUnalignedArray<N, T> {
+    fn default() -> Self {
+        Self([T::default(); N])
+    }
+}
+
+impl<const N: usize, T> AsMut<[T; N]> for DefaultableUnalignedArray<N, T> {
+    fn as_mut(&mut self) -> &mut [T; N] {
+        &mut self.0
+    }
+}
+
+impl<const N: usize, T> AsRef<[T; N]> for DefaultableUnalignedArray<N, T> {
+    fn as_ref(&self) -> &[T; N] {
+        &self.0
+    }
+}
+
+impl<const N: usize, T> AsRef<[T]> for DefaultableUnalignedArray<N, T> {
+    fn as_ref(&self) -> &[T] {
+        self.0.as_slice()
+    }
+}
+
+impl<const N: usize, T> AsMut<[T]> for DefaultableUnalignedArray<N, T> {
+    fn as_mut(&mut self) -> &mut [T] {
+        self.0.as_mut_slice()
+    }
+}
+
 struct RngBufferCore<const N: usize, T: Rng>(T);
 
 impl <const N: usize, T: Rng> BlockRngCore for RngBufferCore<N, T> where [(); WORDS_PER_STD_RNG * N]: {
     type Item = u64;
-    type Results = DefaultableAlignedArray<{ WORDS_PER_STD_RNG * N }, u64>;
+    type Results = DefaultableUnalignedArray<{ WORDS_PER_STD_RNG * N }, u64>;
 
     fn generate(&mut self, results: &mut Self::Results) {
         self.0.fill_bytes(cast_slice_mut(results.as_mut()));
