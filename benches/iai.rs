86904c3743b45b073a682f332423fe3da5b71d24
Chris Hennick <hennickc@amazon.com>
2024-01-20 17:50:59 -0800

    Bug fix

--- benches/iai.rs
+++ benches/iai.rs
@@ -46,10 +46,10 @@ macro_rules! contended_bench_iai {
             fn [< contended_bench_ $n _shared_buffer >]() {
                 let iterations_left = Arc::new(AtomicU64::new(2 * RESEEDING_THRESHOLD * $n.max(1)));
                 let root = BenchmarkSharedBufferRng::<$n>::new(OsRng::default());
-                let rngs: Vec<_> = (0..$threads)
+                let mut rngs: Vec<_> = (0..$threads)
                     .map(|_| root.new_standard_rng(RESEEDING_THRESHOLD))
                     .collect();
-                let main_thread_rng = rngs.pop().unwrap();
+                let mut main_thread_rng = rngs.pop().unwrap();
                 drop(root);
                 let background_threads: Vec<_> = rngs.into_iter()
                     .map(|mut rng| {
@@ -68,7 +68,7 @@ macro_rules! contended_bench_iai {
 
             fn [< contended_bench_ $n _local_buffer >]() {
                 let iterations_left = Arc::new(AtomicU64::new(2 * RESEEDING_THRESHOLD * $n.max(1)));
-                let rngs: Vec<_> = (0..$threads)
+                let mut rngs: Vec<_> = (0..$threads)
                     .map(|_| {
                         let mut buffer = BlockRng64::new(RngBufferCore::<$n, OsRng>(OsRng::default()));
                         let mut seed = [0u8; 32];
@@ -76,7 +76,7 @@ macro_rules! contended_bench_iai {
                         ReseedingRng::new(ChaCha12Core::from_seed(seed), RESEEDING_THRESHOLD, buffer)
                     })
                     .collect();
-                let main_thread_rng = rngs.pop().unwrap();
+                let mut main_thread_rng = rngs.pop().unwrap();
                 let background_threads: Vec<_> = rngs.into_iter()
                     .map(|mut rng| {
                         let iterations_left = iterations_left.clone();
